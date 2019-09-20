fff13673a73092d49f7439ecc1df6f07f4409d28
JDoGlass <jim.zuniga@ucr.ac.cr>
2019-09-20 13:57:11 -0600

    Transmisión confiable: Clarificación del código de envío.

--- transmision-confiable/tcp_light.py
+++ transmision-confiable/tcp_light.py
@@ -7,7 +7,7 @@ import time
 
 DATA_MAX_SIZE = 1 + 4 + 512
 RECEIVE_TIMEOUT = 0.1
-FINAL_MESSAGE = gen_mensaje_final()
+FINAL_MESSAGE = generar_mensaje_final()
 
 #UDP_IP = raw_input("Escriba el numero de ip ")
 UDP_PORT = input("Escriba el numero de puerto: ")
@@ -17,7 +17,7 @@ mi_socket.bind(('',int(UDP_PORT)))#Recibe dos valores que uno es el host y el ot
 
 #mi_socket.listen(5)#Cantidad de peticiones que puede manejar un socket.(conexiones que puede tener en cola.)
 
-def gen_mensaje_final():
+def generar_mensaje_final():
 	msj_final = bytearray(DATA_MAX_SIZE)
 	msj_final[4] = 42
 	return msj_final
@@ -58,12 +58,7 @@ def recibir():
 def enviar():
 	# se aplica un timeout para el socket.
 	mi_socket.settimeout(RECEIVE_TIMEOUT)
-	
-	mensaje = bytearray(DATA_MAX_SIZE)
-	respuesta = bytearray(DATA_MAX_SIZE)
-	
-	mensaje_final = 
-	
+
 	while True:
 		ipPort = input("IP/Port: ") #lee una linea
 		ipPortLine = ipPort.split() #split a la linea
@@ -72,52 +67,54 @@ def enviar():
 		#print(ip, port)
 
 		#nocion de conexión
-		mensaje = bytearray(DATA_MAX_SIZE) # 0 000 0...
-		mi_socket.sendto(mensaje,(ip, int(port))) #establecer conexión
+		mensaje_por_enviar = bytearray(DATA_MAX_SIZE) # 0 000 0...
+		mi_socket.sendto(mensaje_por_enviar,(ip, int(port))) #establecer conexión
 
 		in_file = open("in-small.jpg", "rb") #abrir imagen
 
-		#packetCounter = 0 #para control
-		#windowCounter = 0
+		# indice del paquete que se está enviando
+		packet_counter = 0 
 		
 		SN = 0
 		RN = 0
 
 		image_slice = in_file.read(512) #lee 512 bytes de la imagen
 
-		
+		trabajo_pendiente = true
 
 		while image_slice: #mientras hayan partes de la imagen por enviar
-		    mensaje[4:4+len(image_slice)] = image_slice #copia la porcion de la imagen en el mensaje
+
+		    mensaje_por_enviar[4:4+len(image_slice)] = image_slice #copia la porcion de la imagen en el mensaje
 		    
 		    #setear encabezado
-		    mensaje[0] = 0x00
-		    mensaje[1:3] = SN.to_bytes(3, byteorder='big')
+		    mensaje_por_enviar[0] = 0x00
+		    mensaje_por_enviar[1:3] = SN.to_bytes(3, byteorder='big')
 		    
-		    mi_socket.sendto(mensaje,(ip, int(port))) #enviar paquete
+		    mi_socket.sendto(mensaje_por_enviar,(ip, int(port))) #enviar paquete
 		    
 		    try:
 				respuesta, addr = mi_socket.recvfrom(DATA_MAX_SIZE)
+				answer_number = int.from_bytes(bytearray(respuesta[1:3]), byteorder='little') ############## Little endian
+
+				# if the receiver is asking for the next package:
+				if (answer_number == packet_counter + 1)
+					packet_counter += 1
+					image_slice = in_file.read(512)
+
 			except socket.timeout:
 				# no se recibio ningun paquete
-				
-			
-			#recibir próximo paquete a enviar y recibir su numero
-			#nextPacket = int(mensajeRecibido[1:4])
-			#if nextPacket in window
-			#	mi_socket.sendto(window[nextPacket],(ip, int(port)))
-		    
-		    image_slice = in_file.read(512) #tomar siguiente porciòn de la imagen
+				# por esto, se vuelve a intentar enviar el paquete actual
 		in_file.close()
 		
-hilo1 = threading.Thread(target=enviar)#Definimos la tarea de nuestro hilo.
+hilo_de_envio = threading.Thread(target=enviar)#Definimos la tarea de nuestro hilo.
 
 
+# "Main"
 while True:
     #Esto nos genera dos valores, la conexion y la direcion 
     	#conexion, direccion = mi_socket.accept()#aqui aceptamos la peticiones.
 	#mi_socket.bind((UDP_IP, UDP_PORT))
-	hilo1.start()#Empezamos la ejecucion del hilo
+	hilo_de_envio.start()#Empezamos la ejecucion del hilo
     #mi_socket.setblocking(0)
 	while True: #Lee lo que el usuario digita hasta que se presiona enter.
 		respuesta, addr = mi_socket.recvfrom(1024) # para recibir lo que el servidor nos quiera enviar 
