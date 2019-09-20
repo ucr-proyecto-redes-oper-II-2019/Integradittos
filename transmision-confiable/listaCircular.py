703bb0afd061c60601d018e5d81642dd6d415716
Isaac <isaac.tm@hotmail.com>
2019-09-20 16:17:40 -0600

    Lista circular v2.

--- transmision-confiable/listaCircular.py
+++ transmision-confiable/listaCircular.py
@@ -6,22 +6,25 @@ class listaCirular():
 	int inicio = 0 
 	int final = 0
 	int numeroDePaqueteActual = 0
+	#Se establece la posicion de inicio de la lista que este correspode al RN. 
+	def establecerInicio(numeroDePaqueteInicio):
+		numeroDePaqueteActual = numeroDePaqueteInicio		
+		inicio = numeroPaqueteInicio % 10
 
-	def establecerInicio(int nuevoInicio, int numeroDePaquete):#establecemos el nuevo inicio desde nuestra lista.
-		inicio = nuevoInicio
-		numeroDePaqueteActual = numeroDePaquete
-
+	#Inserta un elemento en la lista, datos(los 516 bytes del paquete) y numeroDePaqueteAInsertar(El numero del paquete que se va a insertar).
 	def insertar(datos ,int numeroDePaqueteAInsertar):#introducimos el nuevoPaquete
 		posicionParaInsertar = (numeroDePaquete - numeroDePaqueteActual) + inicio
 		posicionParaInsertar = posicionParaInsertar - 10
 		listaCircular[posicionParaInsertar] = datos
-
-	def getElemento(int numeroDePaquete)#Obtener un elemento del arreglo, si no se encuentra se retorna un -1
+		if posicioParaInsertar > final:
+			final =  posicioParaInsertar 
+	#retorna el los datos del numero de paquete, numeroDePaquete si no se encuentra retorna -1
+	def getElemento(int numeroDePaquete)
 		posicionABuscar = ((numeroDePaquete - numeroDePaqueteActual) + inicio) - 10#Calculamos la posicion en la que deberia estar ese paquete en el array circular
 		bytes = listaCircular[posicionABuscar]
 		bytes = bytes[1:3]#Sacamos los bytes donde se encuentra el numero de paquete.
 		int.from_bytes(bytes, byorder='big')
-		if(bytes == numeroDePaquete):#comparamos que el numero de paquete deseado sea el que se encuentre en la posicion. 
+		if bytes == numeroDePaquete:#comparamos que el numero de paquete deseado sea el que se encuentre en la posicion. 
 			return listaCircular[posicionABuscar]
 		else: 
 			return -1
