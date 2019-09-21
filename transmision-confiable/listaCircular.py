47ecb3fc12d97831868b6d0d2823c57fefbde54d
Fabian Antonio Rojas Masis <fabian.rojasmasis@ucr.ac.cr>
2019-09-20 23:14:40 -0600

    listaCircular v4

--- transmision-confiable/listaCircular.py
+++ transmision-confiable/listaCircular.py
@@ -1,8 +1,8 @@
 import numpy as np 
-#Fabian e Isaa
+#Fabian e Isaac
 class listaCirular():
 
-	list listaCircular 
+	list listaCircular = [0,0,0,0,0,0,0,0,0,0]
 	int inicio = 0 
 	int final = 0
 	int numeroDePaqueteActual = 0
@@ -11,18 +11,23 @@ class listaCirular():
 	def establecerInicio(numeroDePaqueteInicio):
 		numeroDePaqueteActual = numeroDePaqueteInicio		
 		inicio = numeroPaqueteInicio % 10
-
+	
 	#Inserta un elemento en la lista, datos(los 516 bytes del paquete) y numeroDePaqueteAInsertar(El numero del paquete que se va a insertar).
 	def insertar(datos ,int numeroDePaqueteAInsertar):#introducimos el nuevoPaquete
-		posicionParaInsertar = (numeroDePaqueteAInsertar - numeroDePaqueteActual) + inicio
-		posicionParaInsertar = posicionParaInsertar - 10
-		listaCircular[posicionParaInsertar] = datos
+		if numeroDePaqueteAInsertar >= 10:#Si el paquete a insertar es mayor a 10 tenemos que aplicar el algoritmo para que la lista sea circular.
+			posicionParaInsertar = ((numeroDePaqueteAInsertar - numeroDePaqueteActual) + inicio)- 10
+		else:#Sino dejamos el numero de paqueta original
+			posicioParaInsertar = numeroDePaqueteAInsertar
+		listaCircular[posicionParaInsertar] = datos#Insertamos los bytes
 		if posicioParaInsertar > final:
 			final =  posicioParaInsertar 
 
 	#retorna el los datos del numero de paquete, numeroDePaquete si no se encuentra retorna -1
 	def getElemento(int numeroDePaquete)
-		posicionABuscar = ((numeroDePaquete - numeroDePaqueteActual) + inicio) - 10#Calculamos la posicion en la que deberia estar ese paquete en el array circular
+		if numeroDePaquete >= 10:
+			posicionABuscar = ((numeroDePaquete - numeroDePaqueteActual) + inicio) - 10#Calculamos la posicion en la que deberia estar ese paquete en el array circular
+		else:
+			posicionABuscar = numeroDePaquete
 		dataBytes = listaCircular[posicionABuscar]
 		dataBytes = dataBytes[1:3]#Sacamos los bytes donde se encuentra el numero de paquete.
 		int.from_bytes(dataBytes, byorder='big')
