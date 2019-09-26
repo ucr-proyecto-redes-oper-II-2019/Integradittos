72bef5d83d992bbc2c1fedc236a3941905556f62
Fabian Antonio Rojas Masis <fabian.rojasmasis@ucr.ac.cr>
2019-09-25 20:29:07 -0600

    Implementacion de los mentodos faltantes.

--- transmision-confiable/Receptor.py
+++ transmision-confiable/Receptor.py
@@ -5,6 +5,7 @@ import select
 import ctypes #para el memset
 import time
 import listaCircular
+from time import time
 
 #Fabian e Isaac. 
 
@@ -20,7 +21,7 @@ class Receptor:
 	UDP_PORT
 	mi_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)#(Por defecto utiliza tcp y ipv4)Nos genera un nuevo socket con los valores por default
 	mi_socket.bind(('',int(UDP_PORT)))#Recibe dos valores que uno es el host y el otro el puerto en el que va a estar esuchando.
-	
+	mensajeNuevo = False 
 	def recibir():#Capa de comunicacion. 
 		while bandera: 
 			paqueteRecibido, direccionIPDelEmisor = mi_socket.recvfrom(516)#Capturamos el datos y tambien la direccion del emisor.
@@ -33,11 +34,15 @@ class Receptor:
 				while paquete != -1 #Mientras el elemento que se quiera este en la lista.
 					almacenar(paquete[4:])#Le mandamos los bytes de la imagen. 
 					ventana.establecerInicio(rv#Establecemos el nuevo inicio de la lista. 
+	
+				mensajeNuevo = True#Se comunican a traves de banderas...  
+			
 			else:#lo guardamos en el buffer. 
 				datosBytes = paqueteRecibido[1:3]
 				int.from_bytes(datosBytes, byorder='big')
 				if rv + 10 > datosBytes#Si esta dentro de la ventana, lo guardamos. 
 					ventana.insertar(paqueteRecibido, datosBytes)
+					
 	def almacenar(datos):#Capa superior
 		imagen = open("laImagen.jpg", "bw")#Abrimos archivo donde vamos a guardar la imagen. 
 		if datos[0] = '*':#Podemos hacer que retorne un False en lugar de jugar con esa variable global. 
@@ -47,5 +52,33 @@ class Receptor:
 			rv++ 
 	
 	def confirmacionDeRecepcion():#Se encarga de enviar los ACK's 
+		while bandera:#Cuando el ciclo de recibir termine este tambien. 
+			tiempoInicio = time()
+			if mensajeNuevo#Si se recibio un mensaje con el rv que se estaba esperando, se activa la bandera. 
+				mensaje = armarMensajeACK()
+				mi_socket.sendto(mensaje,(direccionIPDelEmisor, int(port)))
+			tiempoTotal = time() - tiempoInicio
+			timeout(tiempoTotal)#Revisamos el timeout. 
+			
+	def timeout(tiempo): #Se que lo mas probable es que no lo hagamos con un hilo por separado... ideas? 
+		if tiempo > timeout:#Si el tiempo es mayor al time out debe enviar un paquete de recepcion
+			mi_socket.sendto(
+			mensaje = armarMensajeACK()
+			mi_socket.sendto(ack,(direccionIPDelEmisor, int(port)))#Falta que capturemos cual puero vamos a implementar. 
+
+	def armarMensajeACK():
+		ack[0] = 1#Establecemos que es un mesaje de RV. 
+		numeroDePaquete = rv 
+		ack[1:3] = numeroDePaquete.tobytes(3, byteorder='big')#Pasamos el numero a bytes
+		return ack 
+		
 	
-	def timeout(): #Se que lo mas probable es que no lo hagamos con un hilo por separado... ideas? 
+	
+	
+	
+	
+	
+	
+	
+	
+	
