dc5736b8f8d023d0e3df62e602f87964410d9356
Fabian Antonio Rojas Masis <fabian.rojasmasis@ucr.ac.cr>
2019-09-27 13:55:00 -0600

    Correcion de errores.

--- transmision-confiable/Receptor.py
+++ transmision-confiable/Receptor.py
@@ -25,6 +25,7 @@ mi_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)#(Por defecto utiliz
 mi_socket.bind(('',int(UDP_PORT)))#Recibe dos valores que uno es el host y el otro el puerto en el que va a estar esuchando.
 
 def recibir():#Capa de comunicacion.
+	global rv
 	while bandera:
 		paquete_recibido, direccion_ip_del_emisor = mi_socket.recvfrom(516)#Capturamos el datos y tambien la direccion del emisor.
 		datos_bytes = paquete_recibido[1:4]#extraemos el numero de paquete.
@@ -45,7 +46,8 @@ def recibir():#Capa de comunicacion.
 				ventana.insertar(paquete_recibido, datos_bytes)
 
 def almacenar(datos):#Encargado de guardar el archivo(Capa superior).
-	imagen = open(nombreDelArchivo, "bw")#Abrimos archivo donde vamos a guardar la imagen.
+	global nombre_del_archivo
+	imagen = open(nombre_del_archivo, "bw")#Abrimos archivo donde vamos a guardar la imagen.
 	if datos[0] == '*':#Podemos hacer que retorne un False en lugar de jugar con esa variable global.
 		bandera = False #Para la ejecucion.
 	else:
@@ -57,7 +59,7 @@ def confirmacionDeRecepcion():#Se encarga de enviar los ACK's
 		if mensajeNuevo:#Si se recibio un mensaje con el rv que se estaba esperando, se activa la bandera.
 			candado_critico.acquire()#Empezamos zona critica
 			mensaje = armarMensajeACK()
-			mi_socket.sendto(mensaje,(direccion_ip_del_emisor, int(port)))
+			mi_socket.sendto(mensaje, (direccion_ip_del_emisor, int(UDP_PORT)))
 			mensaje_nuevo = False
 			candado_critico.release()#Fin de la zona critica.
 		tiempo_total = time() - tiempo_inicio
@@ -66,7 +68,7 @@ def confirmacionDeRecepcion():#Se encarga de enviar los ACK's
 def timeout(tiempo):
 	if tiempo > timeout:#Si el tiempo es mayor al time out debe enviar un paquete de recepcion
 		mensaje = armarMensajeACK()
-		mi_socket.sendto(ack,(direccion_ip_del_emisor, int(UDP_PORT)))#Falta que capturemos cual puero vamos a implementar.
+		mi_socket.sendto(mensaje, (direccion_ip_del_emisor, int(UDP_PORT)))#Falta que capturemos cual puero vamos a implementar.
 
 def armarMensajeACK():#Metodo que arma los mensajes.
 	ack = bytearray(DATA_MAX_SIZE)
