79b32fa2b6a9adda5ec85b8a98e96c62e27e88b5
Isaac Tretta <isaac.tretta@ucr.ac.cr>
2019-09-27 00:30:41 -0600

    Acomodo de variables y agregado de actualziacion de variables

--- transmision-confiable/Receptor.py
+++ transmision-confiable/Receptor.py
@@ -18,38 +18,35 @@ class Receptor:
 	direccionIPDelEmisor#La direccion ip del emisor. 
 	puertoDelCliente#Puerto del cliente :). 
 	#estoy generando un servidor con sockets. 
-	UDP_PORT
-	mi_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)#(Por defecto utiliza tcp y ipv4)Nos genera un nuevo socket con los valores por default
-	mi_socket.bind(('',int(UDP_PORT)))#Recibe dos valores que uno es el host y el otro el puerto en el que va a estar esuchando.
 	mensajeNuevo = False 
 	def recibir():#Capa de comunicacion. 
 		while bandera: 
 			paqueteRecibido, direccionIPDelEmisor = mi_socket.recvfrom(516)#Capturamos el datos y tambien la direccion del emisor.
 			datosBytes = paqueteRecibido[1:3]#extraemos el numero de paquete. 
-			int.from_bytes(datosBytes, byorder='big')
+			int.from_bytes(datosBytes, byteorder='big')
 			if rv == datosBytes:#Si es igual al numero de paquete que estamos esperando. 
 				almacenar(paqueteRecibido[4:])#se lo debemos pasar a almacenar.
-				ventana.establecerInicio(rv)#Establecemos el nuevo inicio de la lista. 
+				rv++
 				paquete = ventana.getElemento(rv)
 				while paquete != -1 #Mientras el elemento que se quiera este en la lista.
 					almacenar(paquete[4:])#Le mandamos los bytes de la imagen. 
-					ventana.establecerInicio(rv#Establecemos el nuevo inicio de la lista. 
-	
+					rv++#Establecemos el nuevo inicio de la lista. 
+				ventana.establecerInicio(rv)#Establecemos el nuevo inicio de la lista. 
 				mensajeNuevo = True#Se comunican a traves de banderas...  
 			
 			else:#lo guardamos en el buffer. 
 				datosBytes = paqueteRecibido[1:3]
-				int.from_bytes(datosBytes, byorder='big')
-				if rv + 10 > datosBytes#Si esta dentro de la ventana, lo guardamos. 
+				int.from_bytes(datosBytes, byteorder='big')
+				if rv + 10 > datosBytes and datosBytes > rv#Si esta dentro de la ventana, lo guardamos. 
 					ventana.insertar(paqueteRecibido, datosBytes)
 					
 	def almacenar(datos):#Capa superior
-		imagen = open("laImagen.jpg", "bw")#Abrimos archivo donde vamos a guardar la imagen. 
+		imagen = open("laImagen.jpg", "bw")#Abrimos archivo donde vamos a guardar la imagen. laImagen.bin
 		if datos[0] = '*':#Podemos hacer que retorne un False en lugar de jugar con esa variable global. 
 			bandera = False#Para la ejecucion. 
 		else:
 			imagen.write(datos)
-			rv++ 
+			#rv++ 
 	
 	def confirmacionDeRecepcion():#Se encarga de enviar los ACK's 
 		while bandera:#Cuando el ciclo de recibir termine este tambien. 
@@ -73,8 +70,10 @@ class Receptor:
 		return ack 
 		
 	
-	
-	
+# Ejecucion del programa	
+UDP_PORT = input("Escriba el numero de puerto: ")
+mi_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)#(Por defecto utiliza tcp y ipv4)Nos genera un nuevo socket con los valores por default
+mi_socket.bind(('',int(UDP_PORT)))#Recibe dos valores que uno es el host y el otro el puerto en el que va a estar esuchando.
 	
 	
 	
