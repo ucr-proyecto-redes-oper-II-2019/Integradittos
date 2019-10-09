e501c9e1ddb8f5d48e71af6170c9039a18e8eaa0
Hernan Torres <hernan51198@gmail.com>
2019-10-09 14:00:30 -0600

    Sumatoria de array, incluyendo cantidades impares, version C.

--- tcpl/sumador.c
+++ tcpl/sumador.c
@@ -7,24 +7,50 @@
 int sumar_vector(int * vector, int countThreads)
 {
 	int resultados[countThreads]; 
-	int temporal[countThreads]; 
-	int sumas_de_vectores = countThreads / 2;
-	int cantidad_sumas = countThreads / 2;
+	int sumas_de_vectores = 0;
+	int cantidad_sumas = 0;
+	if( countThreads % 2 == 0 )
+	{
+		sumas_de_vectores = countThreads / 2;
+		cantidad_sumas = countThreads / 2;
+	}
+	else
+	{
+		sumas_de_vectores = countThreads / 2 + 1;
+		cantidad_sumas = countThreads / 2 + 1;
+	}
 
 	memcpy(resultados, vector, countThreads * sizeof(int));
 	
-	
+	int cantidad_elementos = countThreads;
 	for(int i = 0; i < sumas_de_vectores; i++)
 	{ 	
+		printf("Cantidad de sumas en iteracion %d: %d\n", i, cantidad_sumas );
+		int temporal[cantidad_sumas];
+		 
 		#pragma omp parallel for num_threads(cantidad_sumas) shared(resultados)
 		for(int j = 0; j < cantidad_sumas; j++)
 		{
-			printf("hilo %d suma: %d con %d\n", omp_get_thread_num(),resultados[j*2], resultados[j*2+1] );
-			temporal[j] = suma(resultados[j*2], resultados[j*2+1]);
+			if( j == cantidad_sumas - 1 && cantidad_elementos % 2 == 1 )
+			{
+				printf("Hilo %d baja: %d\n", omp_get_thread_num(),resultados[j*2]);
+				temporal[j] = resultados[j*2];
+			}
+			else
+			{	
+				printf("Hilo %d suma: %d con %d\n", omp_get_thread_num(),resultados[j*2], resultados[j*2+1] );
+				temporal[j] = suma(resultados[j*2], resultados[j*2+1]);
+			}
 		}
+		memcpy(resultados, temporal, cantidad_sumas * sizeof(int));
+		cantidad_elementos = cantidad_sumas;
+		printf("La proxima sumatoria tendrá: %d elementos\n", cantidad_elementos);
+		if( cantidad_sumas % 2 == 0 )
+			cantidad_sumas /= 2;
+		else
+			cantidad_sumas = (cantidad_sumas / 2) + 1;
 
-		cantidad_sumas /= 2;
-		memcpy(resultados, temporal, countThreads * sizeof(int));
+		
 	}
 	return resultados[0];
 }
@@ -32,7 +58,7 @@ int sumar_vector(int * vector, int countThreads)
 int main(int argc, char* argv[])
 {
  	int threads = atoi(argv[1]);
-	int vectorInicial[ ] = {2,3,4,5,6,7,8,9};
+	int vectorInicial[ ] = {1,2,3,4,5};
 	
 	int resultado = sumar_vector(vectorInicial,threads);
 	printf("%d\n", resultado);
