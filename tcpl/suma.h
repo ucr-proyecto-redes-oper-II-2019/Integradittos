45c507857c7007a04948e4874217c12b9d089303
Hernan Torres <hernan51198@gmail.com>
2019-10-09 16:45:40 -0600

    El codigo que provee la funcion suma es capaz de registrar cuando se llama alguna funcion.

--- tcpl/suma.h
+++ tcpl/suma.h
@@ -2,5 +2,6 @@
 #define SUMA
 
 int suma(int x, int y);
+int guardar_registro_funcion(char* nombre);
 
 #endif //SUMA
--- tcpl/sumador.c
+++ tcpl/sumador.c
@@ -4,31 +4,58 @@
 #include <string.h>
 #include "suma.h"
 
-int sumar_vector(int * vector, int countThreads)
+#define MAX_LINEA 2048
+#define MIN(a,b) ( (a) > (b) ? (b) : (a) )
+
+void leer_array(FILE * archivo, int * arreglo, int * cant_elementos)
 {
-	int resultados[countThreads]; 
+	char * linea;
+	size_t size = MAX_LINEA;
+	getline(&linea, &size, archivo);
+	linea[strcspn(linea, "\n")] = 0;
+	char *pt;
+    pt = strtok (linea,",");
+    *cant_elementos = atoi(pt);
+    pt = strtok (NULL, ",");
+    int contador_elementos = 0;
+    while (pt != NULL) 
+    {
+        arreglo[contador_elementos++] = atoi(pt);
+        pt = strtok (NULL, ",");
+    }
+    //return contador_elementos;
+}
+    
+
+
+int sumar_vector(int * vector, int elementos)
+{
+	size_t thread_count = omp_get_max_threads();
+
+	int resultados[elementos]; 
 	int sumas_de_vectores = 0;
 	int cantidad_sumas = 0;
-	if( countThreads % 2 == 0 )
+	if( elementos % 2 == 0 )
 	{
-		sumas_de_vectores = countThreads / 2;
-		cantidad_sumas = countThreads / 2;
+		sumas_de_vectores = elementos / 2;
+		cantidad_sumas = elementos / 2;
 	}
 	else
 	{
-		sumas_de_vectores = countThreads / 2 + 1;
-		cantidad_sumas = countThreads / 2 + 1;
+		sumas_de_vectores = elementos / 2 + 1;
+		cantidad_sumas = elementos / 2 + 1;
 	}
 
-	memcpy(resultados, vector, countThreads * sizeof(int));
+	memcpy(resultados, vector, elementos * sizeof(int));
 	
-	int cantidad_elementos = countThreads;
+	int cantidad_elementos = cantidad_sumas;
 	for(int i = 0; i < sumas_de_vectores; i++)
 	{ 	
 		printf("Cantidad de sumas en iteracion %d: %d\n", i, cantidad_sumas );
 		int temporal[cantidad_sumas];
-		 
-		#pragma omp parallel for num_threads(cantidad_sumas) shared(resultados)
+
+		thread_count = MIN(thread_count, cantidad_sumas);
+		#pragma omp parallel for num_threads(thread_count) shared(resultados, temporal, cantidad_elementos)
 		for(int j = 0; j < cantidad_sumas; j++)
 		{
 			if( j == cantidad_sumas - 1 && cantidad_elementos % 2 == 1 )
@@ -57,12 +84,27 @@ int sumar_vector(int * vector, int countThreads)
 
 int main(int argc, char* argv[])
 {
- 	int threads = atoi(argv[1]);
-	int vectorInicial[ ] = {1,2,3,4,5};
+	FILE * archivo;
+ 	// int threads = atoi(argv[1]);
+ 	if( argc > 1)
+ 	{
+ 		archivo = fopen(argv[1], "rb");
+ 		if(archivo == NULL)
+ 		{
+ 			printf("No se encontro archivo: %s\n", argv[0]);
+ 		}
+ 	}
+ 	else
+ 	{
+ 		printf("Uso: ./sumar archivo.txt\n");
+ 	}	
+ 		
+	int * vectorInicial;
+	int cantidad_elementos = 0;
+	leer_array(archivo, vectorInicial, &cantidad_elementos);
 	
-	int resultado = sumar_vector(vectorInicial,threads);
+	int resultado = sumar_vector(vectorInicial,cantidad_elementos);
 	printf("%d\n", resultado);
+	fclose(archivo);
 	return 0;
 }
-
-
