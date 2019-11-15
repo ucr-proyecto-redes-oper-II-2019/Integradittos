82cc6ad3dfd56440b4c1f10ca437e2e8e423369c
Isaactm3 <isaac.tm@hotmail.com>
2019-11-15 16:35:27 -0600

    Agregado de método que elimina paquetes, falta completar.

--- tcpl/tcpl.py
+++ tcpl/tcpl.py
@@ -1,23 +1,25 @@
-TCPL_MAX_MESSAGE_SIZE = 1024 # bytes. To be decided!
+TCPL_MAX_MESSAGE_SIZE = 1014 # bytes. To be decided!
 TCPL_TIMEOUT = 10 # ms. To be decided!
-
+TCPL_SLEEP_THREAD = 2
 
 
 class TCPL:
 ''' Clase para realizar envío y recepción de paquetes usando el protocolo TCPL. '''
-
+    
+    
 	## 
 	def __init__(self):
 	''' Constructor de objetos de clase TCPLSender '''
-		# List/ Array
-		self._sendingBag = []
-		# List/ Array
-		self._receivingBag = []
+		# Diccionario
+		self._sendingBag = {[]}
+		# Diccionario
+		self._receivingBag = {[]}
 
 	## 
 	def sendPackage(package, address):
 	''' Envía el paquete package a la dirección IP v4 y número de puerto 
 	específicados en address. Retorna 0 si el envío fue exitoso. '''
+	    
 		return 0
 
 	def receivePackage():
@@ -26,9 +28,9 @@ class TCPL:
 		return 0
 	
 
-	
-
-
+	def erasePackage(clave):
+        self._sendingBag.pop(clave)
+        
 
 #  _     _ _ _
 # ACK  packNumber
