31a29f63cf283e6e1f58d42d14ca89758f094fc0
Fabian Antonio Rojas Masis <fabian.rojasmasis@ucr.ac.cr>
2019-11-16 13:10:49 -0600

    se separo logica de armado de mensajes completamente del nodo naranja.

--- AssemblePackagesFactory.py
+++ AssemblePackagesFactory.py
@@ -1,5 +1,6 @@
 
 class AssemblePackageFactory:
+    DATA_MAX_SIZE = 1009
     numberRequestPos = 0
     inicioConfirmacionRespuestaPos = 4
     tareaARealizarPos = 6
@@ -8,6 +9,8 @@ class AssemblePackageFactory:
     tamInicioConfirmacioRespuesta = 2
     tamTareaARealizar = 1
     tamPrioridad = 2
+
+    tamIPyPuerto = 8
 
     def assemblePackage(self, numeroDeRequest, inicioConfirmacionRespuesta, tareaARealizar, tamanoCuerpoPrioridad, datos = 0):
         """
@@ -43,3 +46,53 @@ class AssemblePackageFactory:
         tamanoCuerpoPrioridad = int.from_bytes(paquete[self.tamCuerpoPrioridad:self.tamCuerpoPrioridad + self.tamPrioridad], byteorder='big')
         datos = int.from_bytes(paquete[self.tamCuerpoPrioridad + self.tamPrioridad:], byteorder='big')
         return numeroDeRequest, inicioConfirmacionRespuesta, numeroDeServicio, tamanoCuerpoPrioridad, datos
+
+    def assemblePackageRequestACK(self, packageRequest, estaInstanciado):
+        """
+            Subrutina que emsambla un ACK para un paquete request ACK.
+            @:param packageRequest: paquete request para el que se desea generar respuesta.
+            @:param estaInstanciado boolean que avisa si un nodo ya esta instanciado para poder generar ACK correctamente.
+            :return: paquete respuesta.
+        """
+        packageRequestACK = bytearray(self.DATA_MAX_SIZE)
+        numeroDeServicio = 206
+        estaOcupado = 0
+        estaLibre = 1
+        numeroRequest, idNodoAInstanciar, tareaARealizar, prioridad, datos= self.unpackPackage(packageRequest)
+        if estaInstanciado is True:
+            packageRequestACK = self.assemblePackage(numeroRequest, estaOcupado, numeroDeServicio,0, 0)
+        else:
+            packageRequestACK = self.assemblePackage(numeroRequest, estaLibre, numeroDeServicio, 0, 0)
+        return packageRequestACK
+
+    def assemblePackageRequestPos(self, numeroDeNodoAInstanciar, nodoNaranjaID):
+        """
+            Subrutina que ensambla un paquete de request_pos
+            :return: paquete requestPos ya ensamblado.
+        """
+        numeroDeServicio = 205
+        return self.assemblePackage(numeroDeServicio, numeroDeNodoAInstanciar, nodoNaranjaID, 0, 0)
+
+    def assemblePackageConnectACK(self, paqueteConnect, idNodoAInstanciar, listaVecinos):
+        """
+            Subrutina que arma un ack par un paquete de tipo connect
+            :param paqueteConnect:  paquete connect recibido
+            :param idNodoAInstanciar: id del nodo que se aprobo su instanaciacion.
+            :param listaVecinos: lista de vecinos del id del nodo que se va a instancias.
+            :return: lista de paquetes que se le debe enviar al que solicito el request.
+        """
+        numeroDeServicio = 201
+        packageRequestACK = bytearray(self.DATA_MAX_SIZE)
+        listaDePaquetesConnectACK = []
+        numeroDeServicio = 206
+        estaOcupado = 0
+        estaLibre = 1
+        numeroRequest, idNodoAInstanciar, tareaARealizar, prioridad, datos = self.unpackPackage(paqueteConnect)
+        # Despues de realizar to do el proceso de de reserve de nodos, para este punto debo contar con el ip y el ppuerto del verde, ademas del ID que se negocio con los demas naranjas.
+        for i in listaVecinos:
+            listaDePaquetesConnectACK.append(self.assemblePackage(numeroRequest, idNodoAInstanciar, numeroDeServicio, self.tamIPyPuerto, i)) #Aqui falta que lisrt
+        return listaDePaquetesConnectACK
+
+    def assemblePackageConfirmPos(self):
+
+    def assemblePackageInstanciado(self):
--- OrangeNode.py
+++ OrangeNode.py
@@ -8,35 +8,6 @@ class OrangeNode:
     listOrangeNodes = []
     def __init__(self, id):
         self.id = id
-    def assemblePackageRequestPos(self):
-        """
-            Subrutina que ensambla un paquete de request_pos
-            :return: paquete requestPos ya ensamblado.
-        """
-        numeroDeServicio = 205
-        nodoLibre = self.getNodoNoInstanciado()
-        return AssemblePackageFactory.assemblePackage(numeroDeServicio, nodoLibre, self.id)
-
-    def assemblePackageRequestACK(self, paquete):
-        """
-            Subrutina que emsambla un ACK para un paquete request ACK.
-            :param paquete: paquete request para el que se desea generar respuesta.
-            :return: paquete respuesta.
-        """
-        numeroDeServicio = 206
-        estaOcupado = 0
-        estaLibre = 1
-        numeroRequest, idNodoAInstanciar, tareaARealizar, prioridad = self.unpackPackage(paquete)
-        if self.revisarNumeroDeNodo(idNodoAInstanciar):
-            AssemblePackageFactory.assemblePackage(numeroRequest, estaOcupado, numeroDeServicio, 0)
-        else:
-            AssemblePackageFactory.assemblePackage(numeroRequest, estaLibre, numeroDeServicio, 0)
-
-    def assemblePackageConnectACK(self):
-
-    def assemblePackageConfirmPos(self):
-
-    def assemblePackageInstanciado(self):
 
     def revisarNumeroDeNodo(self, numeroDeNodo):
         """'Si devuelve un true es que el nodo ya ha sido instanciado. """
