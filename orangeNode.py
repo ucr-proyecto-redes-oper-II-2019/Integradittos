7d5096df4531104ae60b76f39cd4a3369d9c06c7
Jim Zuñiga <jim.zuniga@ucr.ac.cr>
2019-11-25 20:07:34 -0600

    Bugs fixed when loading green graph

--- orangeNode.py
+++ orangeNode.py
@@ -1,29 +1,26 @@
+import csv
+
 # Construye el grafo de nodos verdes para un nodo naranja a partir del
 # archivo csv en la ruta filePath. 
 # Retorna un diccionario donde la llave indica el número de un nodo y
 # el valor corresponde a la lista de números de los nodos adyacentes.
 def OrangeLoadGraph(filePath):
 	graphFile = open (filePath, 'r') 
-	readLine = graphFile.readline()
-
+	reader = csv.reader(graphFile)
+	
 	# Diccionario vacío
 	graphDictionary = dict()
-
-	while readLine:
-		# Separa la línea leída usando comas
-		splitLine = readLine.split(",")
-		# El nodo actual es el primero en la línea
-		currentNode = int(splitLine[0])
-		# Los nodos adyacentes son los demás
-		adyacentNodes = [int(node) for node in splitLine[1:]]
-		# Agrega los datos del nodo actual al diccionario
-		graphDictionary[currentNode] =  adyacentNodes
-
-		# Lee la siguiente línea
-		readLine = graphFile.readline()
+	
+	c = 1
+	# Build dictionary
+	for row in reader:
+		print (c)
+		c += 1
+		graphDictionary[row[0]] = row[1:]
+	
+	readLine = graphFile.readline()
 
 	graphFile.close()
-
 	return graphDictionary
 
 # Carga las IP's de los nodos naranja a partir del archivo csv con ruta filePath.
