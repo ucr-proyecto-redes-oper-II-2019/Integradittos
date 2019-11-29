eb340094daac25906bc5708a41e20a15eeada901
JDoGlass <jim.zuniga@ucr.ac.cr>
2019-11-29 16:42:26 -0600

    Implementación de métodos en FileSystem.

--- fileSystem.py
+++ fileSystem.py
@@ -28,6 +28,9 @@ class FileSystem:
 		# Directory in the real OS where file data is put
 		self.fullDirName = "./" + FileSystem.BASE_FILE_DIRECTORY + "/" + nodeIdentifier
 
+		# Number for the next file/ file fragment
+		self.fileCounter = 1000
+
 		# Crea el directorio para almacenar archivos
 		if not os.path.exists(self.fullDirName):
 			os.makedirs(self.fullDirName)
@@ -44,40 +47,88 @@ class FileSystem:
 	'''
 	Almacena un archivo.
 	'''
-	def storeFile (self, fileName, data):
-		pass
+	def storeFile (self, fileName, fileData):
+		# hex returns a string that starts with "0x", so remove those characters.
+		hexadecimalName = hex(self.fileCounter)[2:]
+
+		# Get the path to which the new file will be stored.
+		internalFileName = self.fullDirName + "/" + hexadecimalName + self.FILE_EXTENSION
+
+		# Create, open, write to, and close the file 
+		newFile = open(internalFileName, "wb")
+		newFile.write(fileData)
+		newFile.close()
+
+		# Increases the file counter so the next one may not use the same internal fileName
+		self.fileCounter += 1
+		
+		# Add the file to the table.
+		self.fileTable[fileName] = internalFileName
 	
+
 	'''
 	Recupera un archivo.
 	'''
 	def loadFile (self, fileName):
+		pass
+		#newFile = open(self.fileTable[fileName])
+		#return 
+
+	'''
+	Elimina un archivo.
+	'''
+	def deleteFile (self, fileName):
 		pass
 		
 	'''
 	Determina si un archivo se encuentra en el sistema.
 	'''
 	def findFile(self, fileName):
-		pass
+		return fileName in self.fileTable
 	
 	'''
 	Almacena un fragmento de archivo.
 	'''
-	def storeFragment (self, fragmentName, data):
-		pass
+	def storeFragment (self, fileName, fragmentNo, fragmentData):
+		# hex returns a string that starts with "0x", so remove those characters.
+		hexadecimalName = hex(self.fileCounter)[2:]
+
+		# Get the path to which the new fragment will be stored.
+		internalFileName = self.fullDirName + "/" + hexadecimalName + self.FILE_EXTENSION
+
+		# Create, open, write to, and close the file to store the fragment
+		newFile = open(internalFileName, "wb")
+		newFile.write(fragmentData)
+		newFile.close()
+
+		# Increases the file counter so the next one may not use the same internal fileName
+		self.fileCounter += 1
+		
+		# Add the fragment to the table.
+		if fileName in self.fragmentsTable:
+			self.fragmentsTable[fileName][fragmentNo] = internalFileName
+		else:
+			self.fragmentsTable[fileName] = { fragmentNo : internalFileName }
+		
 	
 	'''
-	Recupera un fragmento de archivo.
+	Recupera un diccionario con los fragmentos de un archivo, indexados según su número
+	de fragmento.
 	'''
-	def loadFragment (self, fragmentName):
-		pass
+	def loadFragments (self, fileName):
+		if fileName in self.fragmentsTable:
+			return self.fragmentsTable[fileName].copy()
+		else:
+			return {}
 		
 	'''
-	Determina si un archivo se encuentra en el sistema.
+	Determina si algún fragmento de un archivo se encuentra en el sistema.
 	'''
 	def findFragment(self, fileName):
+		return fileName in self.fragmentsTable
+
+	'''
+	Elimina un fragmento de archivo.
+	'''
+	def deleteFragment (self, fileName):
 		pass
-	
-	#todo: determinar si estos dos metodos son necesarios:
-	#def listFiles(self):
-	#def listFragments(self):
-		
