7f224b41ec4175f99a9b5a616f52cbeee62ea56f
JDoGlass <jim.zuniga@ucr.ac.cr>
2019-11-29 14:09:40 -0600

    Destructor for FileSystem added.

--- fileSystem.py
+++ fileSystem.py
@@ -1,20 +1,46 @@
 # fileSystem.py
 
+# For directory creation
+import os
+
+# For removing non-empty directories 
+import shutil
+
 '''
-Clase que administra los archivos para un nodo verde.
+Clase que administra los archivos para un nodo verde. En el sistema operativo real, los
+almacena en el directorio "./<greenNodeFiles>/<nodeIdentifier>/"
 '''
 class FileSystem:
+	BASE_FILE_DIRECTORY = "greenNodeFiles"
+	FILE_EXTENSION = ".dat"
+
 	'''
 	Constructor de objetos de clase FileSystem.
 	'''
 	def __init__(self, nodeIdentifier):
 		# Identificador. (Para almacenar los archivos en el sistema real.)
 		self.id = nodeIdentifier
-		# Lista con los nombres de archivos en el sistema de archivos.
-		self.fileTable = []
-		# Lista con los nombres de los fragmentos en el sistema de archivos.
+		# Diccionario con los nombres de archivos en el sistema de archivos, junto con un identificador.
+		self.fileTable = {}
+		# Diccionario con los nombres de los fragmentos en el sistema de archivos, junto con un identificador.
 		self.fragmentsTable = []
-		
+
+		# Directory in the real OS where file data is put
+		self.fullDirName = "./" + FileSystem.BASE_FILE_DIRECTORY + "/" + nodeIdentifier
+
+		# Crea el directorio para almacenar archivos
+		if not os.path.exists(self.fullDirName):
+			os.makedirs(self.fullDirName)
+
+
+	'''
+	Destructor de objetos de clase FileSystem.
+	(Utilizar para limpiar todo rastro de archivos en la computadora usada)
+	'''
+	def __del__(self):
+		shutil.rmtree(self.fullDirName)
+
+
 	'''
 	Almacena un archivo.
 	'''
