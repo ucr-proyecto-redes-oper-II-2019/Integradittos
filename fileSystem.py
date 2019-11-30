e8eacc8263a0530b71f63a46cde546d9bea9aede
Jim Zuñiga <jim.zuniga@ucr.ac.cr>
2019-11-29 22:44:33 -0600

    Todos los métodos de FileSystem implementados

--- fileSystem.py
+++ fileSystem.py
@@ -70,15 +70,29 @@ class FileSystem:
 	Recupera un archivo.
 	'''
 	def loadFile (self, fileName):
-		pass
-		#newFile = open(self.fileTable[fileName])
-		#return 
+		if fileName in self.fileTable:
+			# Obtiene el nombre interno del archivo solicitado, revisando la tabla
+			# de archivos, y abre dicho archivo.
+			newFile = open(self.fileTable[fileName], "rb")
+			# Lee los datos del archivo
+			fileData = bytearray(newFile.read())
+			
+			return fileData
+		else:
+			return bytearray()
 
 	'''
 	Elimina un archivo.
 	'''
 	def deleteFile (self, fileName):
-		pass
+		if fileName in self.fileTable:
+			# Obtiene el nombre interno del archivo solicitado, revisando la tabla
+			# de archivos.
+			internalName = self.fileTable[fileName]
+			# Elimina el archivo con el nombre encontrado
+			os.remove(internalName)
+			# Elimina el archivo de la tabla de archivos
+			self.fileTable.pop(fileName)
 		
 	'''
 	Determina si un archivo se encuentra en el sistema.
@@ -116,9 +130,27 @@ class FileSystem:
 	de fragmento.
 	'''
 	def loadFragments (self, fileName):
+		# Si hay algún fragmento del archivo solicitado,
 		if fileName in self.fragmentsTable:
-			return self.fragmentsTable[fileName].copy()
+			# Crea un diccionario vacío
+			fragments = {}
+
+			# Para cada fragmento almacenado de ese archivo,
+			for fragmentNumber, internalName in self.fragmentsTable[fileName].values():
+				# Se abre y se leen los datos del fragmento
+				internalFile = open(internalName, "rb")
+				data = internalFile.read()
+
+				# Y se guarda el número de fragmento como llave y los datos del fragmento
+				# como valor en el diccionario para retornar
+				fragments[fragmentNumber] = bytearray(data)
+			
+			# Retorna el diccionario con el número y los datos de cada fragmento.
+			return fragments
+
+		# Si no hay fragmentos de ese archivo,
 		else:
+			# Retorna un diccionario vacío
 			return {}
 		
 	'''
@@ -128,7 +160,20 @@ class FileSystem:
 		return fileName in self.fragmentsTable
 
 	'''
-	Elimina un fragmento de archivo.
+	Elimina los fragmentos de un archivo.
 	'''
 	def deleteFragment (self, fileName):
-		pass
+		# Si hay algún fragmento del archivo solicitado,
+		if fileName in self.fragmentsTable:
+
+			# Para cada fragmento almacenado de ese archivo,
+			for fragmentNumber, internalName in self.fragmentsTable[fileName].values():
+				# Se eliminan los datos del fragmento
+				os.remove(internalName)
+
+				# Se elimina el fragmento de la tabla
+				self.fragmentsTable.pop(fragmentNumber)
+			
+			# También se elimina la entrada de la tabla que indicaba la existencia
+			# de fragmentos de un archivo
+			self.fragmentsTable.pop(fileName)
