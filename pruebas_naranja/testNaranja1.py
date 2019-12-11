53a91debfeccab8f0ee0939bcf7b222466fd5f10
Hernan Torres <hernan51198@gmail.com>
2019-12-11 15:22:32 -0600

    Naranajs almost done

---
+++ pruebas_naranja/testNaranja1.py
@@ -0,0 +1,9 @@
+from orangeNode import OrangeNode
+print("Escriba su numero de IP")
+ip = input()
+print("Escriba su numero de puerto.")
+puerto = int(input())
+print("Escriba la prioridad")
+id = int(input())
+orange = OrangeNode(id, ip, puerto)
+orange.start("grafoPrueba.csv", "grafoNaranjas1.txt")
