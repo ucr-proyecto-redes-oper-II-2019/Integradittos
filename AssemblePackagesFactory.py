fbfc1d1b4f7718f267ef57311c5f337dd7465b2a
Fabian Antonio Rojas Masis <b66236@ecci.ucr.ac.cr@ECCI393382L0104.ecci.ucr.ac.cr>
2019-12-16 10:41:56 -0600

    Arreglo a la fabrica de paquetes

--- AssemblePackagesFactory.py
+++ AssemblePackagesFactory.py
@@ -3,7 +3,7 @@ import random
 
 class AssemblePackageFactory:
 
-    def __init__(self): 
+    def __init__(self):
         self.DATA_MAX_SIZE = 1009
         #Posiciones
         self.numberRequestPos = 0
@@ -16,9 +16,12 @@ class AssemblePackageFactory:
         self.tamTareaARealizar = 1
         self.tamPrioridad = 2
         self.tamIPyPuerto = 8
+        self.tamTTL = 2
+        self.tamFuente = 2
+        self.TamDestino = 2
         self.randomMaximo = 65000
 
-    def assemblePackage(self, numeroDeRequest, inicioConfirmacionRespuesta, tareaARealizar, tamanoPrioridad, datos):
+    def assemblePackage(self, numeroDeRequest, inicioConfirmacionRespuesta, tareaARealizar, tamanoPrioridad, TTL, fuente, destino, datos):
         """
             Subrutina que arma un paquete
             @:param numeroDeRequest numero random que identifica una request.
@@ -32,7 +35,10 @@ class AssemblePackageFactory:
         paquete[4:6] = inicioConfirmacionRespuesta.to_bytes(self.tamInicioConfirmacioRespuesta, byteorder='big')
         paquete[6] = tareaARealizar
         paquete[7:9] = tamanoPrioridad.to_bytes(self.tamPrioridad, byteorder='big')
-        paquete[9:] = datos
+        paquete[9:11] = TTL.to_bytes(self.tamTTL, byteorder='big')
+        paquete[11:13] = fuente.to_bytes(self.tamFuente, byteorder='big')
+        paquete[13:15] = destino.to_bytes(self.TamDestino, byteorder='big')
+        datos[15:] = datos
         return paquete
 
     def unpackPackage(self, paquete):
@@ -45,7 +51,10 @@ class AssemblePackageFactory:
         inicioConfirmacionRespuesta = int.from_bytes(paquete[4:6], byteorder='big')
         numeroDeServicio = paquete[6]
         tamanoCuerpoPrioridad = int.from_bytes(paquete[7:9], byteorder='big')
-        datos = paquete[9:]
+        ttl = int.from_bytes(paquete[9:11], byteorder='big')
+        fuente = int.from_bytes(paquete[11:13], byteorder='big')
+        destino = int.from_bytes(paquete[13:15], byteorder='big')
+        datos = paquete[15:]
         return numeroDeRequest, inicioConfirmacionRespuesta, numeroDeServicio, tamanoCuerpoPrioridad, datos
 
     def assemblePackageRequestPos(self, numeroDeNodoAInstanciar, nodoNaranjaID):
@@ -85,50 +94,50 @@ class AssemblePackageFactory:
             :return: lista de paquetes que se le debe enviar al que solicito el request.
         """
         numeroDeServicio = 201
-        listaDePaquetesConnectACK = [] 
+        listaDePaquetesConnectACK = []
         numeroRequest, inicioConfirmacionRespuesta, tareaARealizar, prioridad, datos = self.unpackPackage(paqueteConnect)
         # Despues de realizar to do el proceso de de reserve de nodos, para este punto debo contar con el ip y el ppuerto del verde, ademas del ID que se negocio con los demas naranjas.
         paquete = bytearray(8)
         for vecino in listaVecinos:
             paquete[0:2] = int(vecino.id).to_bytes(2, byteorder="big") #Id del nodo
             arrayIPandPort = self.packIpPort(vecino.ip, vecino.port)
-            paquete[2:6] = arrayIPandPort[0:4] 
+            paquete[2:6] = arrayIPandPort[0:4]
             paquete[6:8] = arrayIPandPort[4:]
-            listaDePaquetesConnectACK.append(self.assemblePackage(numeroRequest, idNodoAInstanciar, numeroDeServicio, self.tamIPyPuerto, paquete)) #Aqui falta que lisrt
+            listaDePaquetesConnectACK.append(self.assemblePackage(numeroRequest, idNodoAInstanciar, numeroDeServicio, self.tamIPyPuerto, 50, 0, 0, paquete)) #Aqui falta que lisrt
         return listaDePaquetesConnectACK
 
     def assemblePackageConfirmPos(self, nodoReservado, ipAndPort):
         numeroDeServicio = 210
         tamanoIP = 4
         tamanoPort = 2
-        return self.assemblePackage(random.randint(0,self.randomMaximo), nodoReservado, numeroDeServicio, tamanoIP+tamanoPort, self.packIpPort(ipAndPort[0],ipAndPort[1]))
+        return self.assemblePackage(random.randint(0,self.randomMaximo), nodoReservado, numeroDeServicio, tamanoIP+tamanoPort, 50, 0, 0, self.packIpPort(ipAndPort[0],ipAndPort[1]))
 
     def assemblePackageInstanciado(self):
         pass
 
-    def assemblePackageConfirmPosACK(self, id, requestNum): 
+    def assemblePackageConfirmPosACK(self, id, requestNum):
         numeroDeServicio = 211
-        return self.assemblePackage(requestNum, id, numeroDeServicio, 0, int(0).to_bytes(1, byteorder='big'))
+        return self.assemblePackage(requestNum, id, numeroDeServicio, 0 , 50, 0, 0, int(0).to_bytes(1, byteorder='big'))
 
-    def assemblePackageGreetNeighbor(self, id):
+    def assemblePackageGreetNeighbor(self, id, destino, fuente):
         numeroDeServicio = 100
-        return self.assemblePackage(random.randint(0,self.randomMaximo), id, numeroDeServicio, 0, int(0).to_bytes(1, byteorder='big'))
+        return self.assemblePackage(random.randint(0,self.randomMaximo), id, numeroDeServicio, 0 , 50, destino, fuenta, int(0).to_bytes(1, byteorder='big'))
 
-    def assemblePackageGreetNeighborACK(self, requestNum):
+    def assemblePackageGreetNeighborACK(self, requestNum, destino, fuente):
         numeroDeServicio = 101
-        return self.assemblePackage(requestNum, 0, numeroDeServicio, 0, int(0).to_bytes(1, byteorder='big'))
+        return self.assemblePackage(requestNum, 0, numeroDeServicio, 0 , 50, destino, fuente, int(0).to_bytes(1, byteorder='big'))
 
     def assemblePackageFileExistACK(self, requestNum, answer):
         numeroDeServicio = 103
-        return self.assemblePackage(requestNum, 0, numeroDeServicio, 0, int(answer).to_bytes(1, byteorder='big'))
+        return self.assemblePackage(requestNum, 0, numeroDeServicio, 0, 50, 0, 0, int(answer).to_bytes(1, byteorder='big'))
 
     def assmblePackageRemoveFileACK(self, requesNum):
         numeroDeServicio = 109
-        return self.assemblePackage(requesNum, 0, numeroDeServicio, 1, int(0).to_bytes(1, byteorder='big'))
+        return self.assemblePackage(requesNum, 0, numeroDeServicio, 1, 50, 0, 0, int(0).to_bytes(1, byteorder='big'))
 
     def assemblePackageConnect(self):
         numeroDeServicio = 200
-        return self.assemblePackage(random.randint(0,self.randomMaximo), 1, numeroDeServicio, 0, int(0).to_bytes(1, byteorder='big'))
+        return self.assemblePackage(random.randint(0,self.randomMaximo), 1, numeroDeServicio, 0, 50, 0, 0, int(0).to_bytes(1, byteorder='big'))
 
 
     def packIP(self, ip):
--- prueba.py
+++ prueba.py
@@ -1,5 +1,5 @@
 import sys
 from greenNode import GreenNode
 import sys
-verde = GreenNode(int(sys.argv[1]), "10.1.137.90", 5555)
+verde = GreenNode(int(sys.argv[1]), sys.argv[2], int(sys.argv[3]))
 verde.run()
